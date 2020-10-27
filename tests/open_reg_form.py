b3940de211b8f8bfb3fb04762b7541d3b0e72f77
Egor Bedov <bedovegor2203@gmail.com>
2020-10-27 16:33:04 +0300

    Refactor repeatable test

--- tests/open_reg_form.py
+++ tests/open_reg_form.py
@@ -8,14 +8,14 @@ from pages.main_page import MainPage, RegForm
 from tests.test_class import Test
 
 
-import unittest
+def open_reg_form(test_case: unittest.TestCase, main_page: MainPage, form: RegForm):
+    main_page.open()
+    form.open()
+    test_case.assertEqual("SIGN UP", form.title_text)
+
 
 class OpenRegTest(Test):
-    TITLE = "SIGN UP"
-
     def test(self):
         main_page = MainPage(self.driver)
-        main_page.open()
         form = main_page.reg_form
-        form.open()
-        self.assertEqual(self.TITLE, form.title_text)
+        open_reg_form(self, main_page, form)
