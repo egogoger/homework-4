5aea1dc4361cd45b6a1bb3eb19525837978d2305
Egor Bedov <bedovegor2203@gmail.com>
2020-10-28 02:05:11 +0300

    Add test for invalid second pass

--- run_tests.py
+++ run_tests.py
@@ -7,14 +7,16 @@ from tests.open_reg_form import OpenRegTest
 from tests.empty_fields import EmptyFieldsTest
 from tests.invalid_name import InvalidNameTest
 from tests.invalid_login import InvalidLoginTest
+from tests.invalid_pass2 import InvalidPassword2Test
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
-        unittest.makeSuite(OpenRegTest),
-        unittest.makeSuite(EmptyFieldsTest),
-        unittest.makeSuite(InvalidNameTest),
-        unittest.makeSuite(InvalidLoginTest)
+        # unittest.makeSuite(OpenRegTest),
+        # unittest.makeSuite(EmptyFieldsTest),
+        # unittest.makeSuite(InvalidNameTest),
+        # unittest.makeSuite(InvalidLoginTest),
+        unittest.makeSuite(InvalidPassword2Test),
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
