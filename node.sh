bb84db578b0af02dd48c831b62dcc220485c65be
Egor Bedov <bedovegor2203@gmail.com>
2020-10-27 12:07:23 +0300

    Add necessary files

---
+++ node.sh
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver="./chromedriver" -Dwebdriver.gecko.driver="./geckodriver" -jar selenium-server-standalone-3.141.59.jar -role node -hub http://localhost:4444/grid/register -browser browserName=chrome,maxInstances=2 -browser browserName=firefox,maxInstances=2
